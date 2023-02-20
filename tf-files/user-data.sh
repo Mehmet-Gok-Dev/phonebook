350b8bd031d46991bfc61b8722455699b0a22d24
Mehmet GOK <76647960+Mehmet-Gok-Dev@users.noreply.github.com>
2023-02-20 11:34:26 +0000

    Update user-data.sh

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="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"
+TOKEN="xxxxxxxxxxxxxxxx"
 cd /home/ec2-user && git clone https://$TOKEN@github.com/Mehmet-Gok-Dev/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
